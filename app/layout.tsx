947e1ea3fcbd7fbda2f9348436576deb2c924fae
Om Mishra <om.works01@gmail.com>
2025-09-09 18:03:38 +0530

    refactor(updates): update next,tailwind versions

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,19 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
+import { Funnel_Display } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const funnel = Funnel_Display({
+  variable: "--font-funnel",
+  weight: ["400", "500", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
   title: "QuickBoarder - AI Product Dashboard",
-  description: "Upload product images and let AI generate product details for your e-commerce store",
+  description:
+    "Upload product images and let AI generate product details for your e-commerce store",
 };
 
 export default function RootLayout({
@@ -26,13 +22,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute={"class"} defaultTheme="dark" enableSystem disableTransitionOnChange>
-        {children}
-        <Toaster />
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${funnel.variable} antialiased`}>
+        <ThemeProvider
+          attribute={"class"}
+          defaultTheme="light"
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
