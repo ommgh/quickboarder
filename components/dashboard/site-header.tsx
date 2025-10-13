9b6e615d18e4aba8006292569a4ef3b350f3bebd
Om Mishra <om.works01@gmail.com>
2025-10-14 02:02:54 +0530

    fix(payments): remove test payments

--- components/dashboard/site-header.tsx
+++ components/dashboard/site-header.tsx
@@ -2,21 +2,17 @@
 import { ModeToggle } from "../mode-toggle";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { SidebarTrigger } from "../ui/sidebar";
-import UpgradeButton from "./Upgrade";
+
 import { signOut, useSession } from "next-auth/react";
 export function SiteHeader() {
   const session = useSession();
   const name = session.data?.user.name || "";
   const avatar = session.data?.user.image || "";
-  const handleSignOut = () => {
-    signOut();
-  };
   return (
     <header className="sticky top-0 z-10 bg-background flex h-16 shrink-0 items-center p-4 md:p-8">
       <SidebarTrigger className="md:hidden" />
 
       <div className="flex-1 flex items-center justify-end gap-2">
-        <UpgradeButton productId="pdt_mfnm0X5FtPNHExUNipgzk" />
         <ModeToggle />
         <Avatar className="h-8 w-8 rounded-lg">
           <AvatarImage src={avatar} alt={name} />
