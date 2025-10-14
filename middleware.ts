5d5b26546e7d8db93536a3f75f22abde6899ac11
Om Mishra <om.works01@gmail.com>
2025-10-14 22:19:20 +0530

    fix(middleware): remove heavy authjs dependencies

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,42 @@
-import NextAuth from "next-auth";
-import authConfig from "@/auth.config";
+import { NextResponse, type NextRequest } from "next/server";
 import {
   DEFAULT_AUTH_REDIRECT,
   privateRoutes,
   authRoutes,
   apiAuthPrefix,
 } from "@/routes";
-const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
-  const { nextUrl } = req;
-  const isLoggedIn = !!req.auth;
+export function middleware(req: NextRequest) {
+  const { nextUrl, cookies } = req;
+
+  const token =
+    cookies.get("next-auth.session-token")?.value ||
+    cookies.get("__Secure-next-auth.session-token")?.value;
+
+  const isLoggedIn = !!token;
+
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   if (isApiAuthRoute) {
-    return;
+    return NextResponse.next();
   }
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      return Response.redirect(new URL(DEFAULT_AUTH_REDIRECT, nextUrl));
-    }
-    return;
+  if (isAuthRoute && isLoggedIn) {
+    return NextResponse.redirect(new URL(DEFAULT_AUTH_REDIRECT, nextUrl));
   }
-  if (!isLoggedIn && isPrivateRoute) {
-    return Response.redirect(new URL("/auth/login", nextUrl));
+
+  if (isPrivateRoute && !isLoggedIn) {
+    return NextResponse.redirect(new URL("/auth/login", nextUrl));
   }
-  return;
-});
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-
     "/(api|trpc)(.*)",
   ],
 };
